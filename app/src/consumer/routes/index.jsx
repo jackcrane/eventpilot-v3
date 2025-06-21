b3ffed0dc7dc5e86c55ecdabcf22ac3f494b12dd
jackcrane <jack@jackcrane.rocks>
2025-06-21 13:45:10 +0000

    Implement EventRegistration component with event fetching and form handling

---
+++ app/src/consumer/routes/index.jsx
@@ -0,0 +1,58 @@
+import { useReducedSubdomain } from "../../../../hooks/useReducedSubdomain";
+import { useEvent } from "../../../../hooks/useEvent";
+import { Typography } from "tabler-react-2";
+import { Row } from "../../../../util/Flex";
+import { useFormBuilder } from "../../../../hooks/useFormBuilder";
+import { FormConsumer } from "../../../../components/formConsumer/FormConsumer";
+import { usePII } from "../../../../hooks/usePII";
+
+export const EventRegistration = () => {
+  const eventSlug = useReducedSubdomain(); // now used as eventId
+  const pii = usePII();
+
+  const { event, loading, error } = useEvent({ eventId: eventSlug });
+  const {
+    fields,
+    loading: loadingForm,
+    error: errorForm,
+    updateFields,
+    submitForm,
+    mutationLoading,
+  } = useFormBuilder(eventSlug);
+
+  if (loading || loadingForm) {
+    return <div>Loading...</div>;
+  }
+
+  if (error || errorForm) {
+    return <div>Error: {error || errorForm}</div>;
+  }
+
+  return (
+    <div style={{ margin: "20px" }}>
+      <Row gap={1}>
+        <img
+          src={event.logo?.location}
+          alt={event.name}
+          style={{
+            width: "80px",
+            height: "80px",
+            borderRadius: "10px",
+            objectFit: "cover",
+          }}
+        />
+        <div>
+          <Typography.H3 className={"mb-0 text-secondary"}>
+            {event.name}
+          </Typography.H3>
+        </div>
+      </Row>
+      <FormConsumer
+        fields={fields}
+        submitForm={submitForm}
+        mutationLoading={mutationLoading}
+        pii={pii}
+      />
+    </div>
+  );
+};
