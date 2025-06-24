955ed308dcf050392951ff764743f653c3d29334
Jack Crane <jack@jackcrane.rocks>
2025-06-24 16:50:16 -0400

    Fix dirname error

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import registerRoutes from "./util/router.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
