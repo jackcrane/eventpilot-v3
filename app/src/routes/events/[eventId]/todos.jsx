c217813c05e43cd5afd0d7db2ca62f4eb664d053
Jack Crane <jack@jackcrane.rocks>
2025-09-07 13:00:35 -0500

    Slightly modify kanban styling

--- app/src/routes/events/[eventId]/todos.jsx
+++ app/src/routes/events/[eventId]/todos.jsx
@@ -5,10 +5,11 @@ import { KanbanBoard } from "../../../../components/KanbanBoard/KanbanBoard";
 export const EventTodosPage = () => {
   return (
     <EventPage title="Todo List" description="Organize tasks across statuses.">
-      <KanbanBoard />
+      <div style={{ height: "calc(100dvh - 260px)" }}>
+        <KanbanBoard />
+      </div>
     </EventPage>
   );
 };
 
 export default EventTodosPage;
-
